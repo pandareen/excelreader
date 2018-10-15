2cff5c2f9c679a939ee257e9129aba7bdabf7f77
pandareen <sandeshyapuram@gmail.com>
2018-10-15 17:59:15 +0530

    changes in shell script

--- scripts/getmailbox.ps1
+++ scripts/getmailbox.ps1
@@ -1,13 +1,17 @@
 #Constant Variables
-$Office365AdminUsername = "admin@stringbase.in"
-$Office365AdminPassword = "Seclore@123"
-$CommandOutputPath = "./output/out.json"
+$Office365AdminUsername = 'admin@stringbase.in'
+$Office365AdminPassword = 'Seclore@123'
+$SharedMailboxList = "./output/sharedmailboxlist.json"
+$SharedMailboxListFullAccessRights = "./output/sharedmailboxpermissionlist.json"
+$SharedMailboxListSendAsRights = "./output/sharedmailboxsendasrightslist.json"
+$SharedMailboxListGrantOnBehalfOf = "./output/sharedmailboxgrantonbehalfoflist.json"
 
 #Main
 Function Main
 {
     #Remove all existing Powershell sessions
     Get-PSSession | Remove-PSSession
+
 
     #Encrypt password for transmission to Office365
     $SecureOffice365Password = ConvertTo-SecureString -AsPlainText $Office365AdminPassword -Force
@@ -18,7 +22,7 @@ Function Main
     Write-Host : 'Credentials object created'
 
     #Create remote Powershell session
-    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https:/ /ps.outlook.com/powershell -Credential $Office365credentials -Authentication Basic -AllowRedirection
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $Office365credentials -Authentication Basic -AllowRedirection
     Write-Host : 'Remote session established'
 
     #Check for errors
@@ -33,9 +37,22 @@ Function Main
         Import-PSSession $Session
     }
 
-    #To check folder size
-    $get_mailbox_output = Get-Mailbox
-    $get_mailbox_output | ConvertTo-Json > $CommandOutputPath
+    $shared_mailboxes = Get-Mailbox | where {$_.recipientTypeDetails -eq 'sharedmailbox' }
+
+    $shared_mailboxes | ConvertTo-Json > $SharedMailboxList
+
+    $shared_mailboxes_permission = $shared_mailboxes | Get-MailboxPermission | where {$_.user.tostring() -ne "NT AUTHORITY\SELF" -and $_.IsInherited -eq $false}
+
+    $shared_mailboxes_permission | ConvertTo-Json > $SharedMailboxListFullAccessRights
+
+    $recipient_permission = Get-Mailbox -ResultSize Unlimited | Get-RecipientPermission | ? {$_.Trustee -ne "NT AUTHORITY\SELF"}
+
+    $recipient_permission | ConvertTo-Json > $SharedMailboxListSendAsRights
+
+    $granted_on_behalf = $shared_mailboxes |  ? {$_.GrantSendOnBehalfTo -ne $null} | select Name,Alias,UserPrincipalName,PrimarySmtpAddress,GrantSendOnBehalfTo
+
+    $granted_on_behalf | ConvertTo-Json > $SharedMailboxListGrantOnBehalfOf
+
     exit
 }
 
