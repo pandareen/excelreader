e43f089856808ef0f91917dfa36a678be8cc5b6b
pandareen <sandeshyapuram@gmail.com>
2018-11-01 09:11:16 +0530

    changes in shell script

--- src/main/java/JPwrshellDemo.java
+++ src/main/java/JPwrshellDemo.java
@@ -1,13 +1,19 @@
 import com.profesorfalken.jpowershell.PowerShell;
 import com.profesorfalken.jpowershell.PowerShellResponse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JPwrshellDemo {
 
     public static void main(String args[])
     {
         PowerShell powerShell = PowerShell.openSession();
 
-        PowerShellResponse response = powerShell.executeScript("scripts/part1.ps1");
+        Map<String, String> myConfig = new HashMap<String, String>();
+        myConfig.put("maxWait", "300000");
+        powerShell.configuration(myConfig);
+        PowerShellResponse response = powerShell.executeScript("scripts/part1.ps1", "$blah='asdasd'");
         System.out.println("Results:" + response.getCommandOutput());
 
 
